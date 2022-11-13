1b7a3d38531a58d97a8119ad72ecfad68a9344b0
Jean Fellipe <jeanfellipesilva93@gmail.com>
2022-11-13 11:04:01 -0300

    add Header module

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Header } from './components/Header'
+
+import './global.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    
+    <div>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import styles from './Header.module.css'
+
+import rocketlogo from '../assets/rocket.svg'
+
+export function Header() {
+    return (
+        <div>
+            <header className={styles.header}>
+                <img src={rocketlogo}/>
+                <span className={styles.title}>
+                    <span className={styles.blueText}>to</span>
+                    <span className={styles.purpleDarkText}>do</span>
+                </span>
+
+            </header>
+
+        </div>
+    )
+}
